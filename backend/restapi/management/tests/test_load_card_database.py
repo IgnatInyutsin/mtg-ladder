e08fd9061b523fbe523c155258410e7d8e358003
IgnatInyutsin <iggyinyutsin@gmail.com>
2022-04-07 20:37:09 +0400

    Написание тестов

---
+++ backend/restapi/management/tests/test_load_card_database.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from restapi.management.commands.load_card_database import Command
+from restapi.app.cards.models import *
+
+class LoadCardDatabaseTest(TestCase):
+    def test_creating_cards(self):
+        # Проверяем, что выполнился без ошибок
+        self.assertEqual(Command.handle(None), True)
+        # Проверяем количество карт
+        self.assertEqual(len(Card.objects.all()) > 8900, True)
+        # Проверяем количество правил
+        self.assertEqual(len(CardRule.objects.all()) > 15000, True)
