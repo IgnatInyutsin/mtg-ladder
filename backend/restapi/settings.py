8a25623a671a23f5e31ace0909ac3ac6b3fe4123
ishao <70522007+IgnatInyutsin@users.noreply.github.com>
2022-04-08 07:28:47 +0400

    Тестовый коммит для github Workflow

--- backend/restapi/settings.py
+++ backend/restapi/settings.py
@@ -4,10 +4,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRETKEY')
 
