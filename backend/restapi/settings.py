3b59f7c6d3f3a91249024fb00b2848b46c6beeac
ishao <70522007+IgnatInyutsin@users.noreply.github.com>
2022-04-08 07:30:21 +0400

    Тестовый коммит

--- backend/restapi/settings.py
+++ backend/restapi/settings.py
@@ -1,13 +1,10 @@
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRETKEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DJANGO_DEBUG')))
 
 ALLOWED_HOSTS = ['*']
